5f3ac325c28af2f9e7769079400402286feb7116
root <root@DESKTOP-EJIAV4T.localdomain>
2025-12-05 03:54:15 -0800

    Add route for blog-post page

--- src/App.jsx
+++ src/App.jsx
@@ -16,6 +16,7 @@ function App() {
         <Route path="/about" element={<AboutPage />} />
         <Route path="/services" element={<ServicesPage />} />
         <Route path="/blog" element={<BlogIndexPage />} />
+        <Route path="/blog-post" element={<BlogPostPage />} />
         <Route path="/contact" element={<ContactPage />} />
       </Routes>
     </MainLayout>
